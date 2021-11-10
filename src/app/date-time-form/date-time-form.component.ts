b871f28efbc13df72f85278be202391ae1502816
anonbnr <bashar.nabil.rima@gmail.com>
2021-11-10 12:13:28 +0100

    Added project

---
+++ src/app/date-time-form/date-time-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-date-time-form',
+  templateUrl: './date-time-form.component.html',
+  styleUrls: ['./date-time-form.component.css']
+})
+export class DateTimeFormComponent implements OnInit {
+
+  dateTimeForm: FormGroup
+  dateTime: string;
+  time: string;
+  month: string;
+  week: string;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(){
+    this.dateTimeForm = this.formBuilder.group({
+      'dateTime': ['', Validators.required],
+      'time': ['', Validators.required],
+      'month': ['', Validators.required],
+      'week': ['', Validators.required],
+    });
+  }
+
+  onSubmit(){
+    this.dateTime = new Date(this.dateTimeForm.get('dateTime').value).toLocaleDateString();
+    this.time = this.dateTimeForm.get('time').value;
+    this.month = this.dateTimeForm.get('month').value;
+    this.week = this.dateTimeForm.get('week').value;
+  }
+
+}
---
+++ src/app/new-post/new-post.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Post } from '../models/post.model';
+import { PostService } from '../services/post.service';
+
+@Component({
+  selector: 'app-new-post',
+  templateUrl: './new-post.component.html',
+  styleUrls: ['./new-post.component.css']
+})
+export class NewPostComponent implements OnInit {
+
+  postForm: FormGroup
+
+  constructor(
+    private postService: PostService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(){
+    this.postForm = this.formBuilder.group({
+      'title': ['', Validators.required],
+      'content': ['', Validators.required]
+    });
+  }
+
+  onSavePost(){
+    this.postService.addPost(new Post(
+      this.postForm.get('title').value,
+      this.postForm.get('content').value
+    ));
+
+    this.router.navigate(['/posts']);
+  }
+
+}
